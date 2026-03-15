61f53322cdd76a47fc27d41341599bf077ad02a0
archaeopterisllc <archaeopteris.llc@gmail.com>
2026-03-15 11:58:26 +0700

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -3,10 +3,10 @@ import { Resend } from 'resend'
 import { NextResponse } from 'next/server'
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
-console.log("Dang ket noi den URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("Dang ket noi den URL:", process.env.SUPABASE_URL);
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
