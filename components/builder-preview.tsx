a486f2eb3705d290fd6f0dd81a40612699e76005
archaeopterisllc <contact@archaeopteris.us>
2026-05-27 15:38:49 +0800

    feat: add Project tab with WebContainer

--- components/builder-preview.tsx
+++ components/builder-preview.tsx
@@ -5,30 +5,17 @@ export default function BuilderPreview({ code }: { code: string }) {
   const iframeRef = useRef<HTMLIFrameElement>(null)
 
   useEffect(() => {
-    const html = `<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8"/>
-  <script src="https://unpkg.com/react@18/umd/react.development.js"></script>
-  <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
-  <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
-  <script src="https://cdn.tailwindcss.com"></script>
-  <style>*{box-sizing:border-box}body{margin:0}</style>
-</head>
-<body>
-  <div id="root"></div>
-  <script type="text/babel">
-    const { useState, useEffect, useRef, useCallback, useMemo } = React;
-    const render = (el) => ReactDOM.createRoot(document.getElementById('root')).render(el);
-    ${code}
-  </script>
-</body>
-</html>`
+  console.log("BuilderPreview rendering, code length:", code.length)
+  const html = `...`
+  console.log("Setting srcdoc...")
+  if (iframeRef.current) {
+    iframeRef.current.srcdoc = html
+    console.log("srcdoc set ✓")
+  } else {
+    console.log("iframeRef is null!")
+  }
+}, [code])
 
-    if (iframeRef.current) {
-      iframeRef.current.srcdoc = html
-    }
-  }, [code])
 
   return (
     <iframe
