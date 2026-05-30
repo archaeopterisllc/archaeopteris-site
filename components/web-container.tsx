4a107822c9359af9366fa71f0276c9b3ebe816fc
archaeopterisllc <contact@archaeopteris.us>
2026-05-31 00:06:56 +0800

    feat: vite + shadcn + json generate setup

--- components/web-container.tsx
+++ components/web-container.tsx
@@ -114,23 +114,23 @@ if (hasPackageJson) {
   addLog('Dependencies installed ✓')
 }
 
-devProcessRef.current?.kill()
+//devProcessRef.current?.kill()
 
-  const devProcess = await wc.spawn('npm', ['run', 'dev'])
-  devProcessRef.current = devProcess
-  devProcess.output.pipeTo(
-    new WritableStream({
-      write(data) {
-        addLog(stripAnsi(data))
-        if (data.includes('ready in') && iframeRef.current && url) {
-          iframeRef.current.src = url
-        }
-      }
-    })
-  )
-  setTimeout(() => {
-    if (iframeRef.current && url) iframeRef.current.src = url
-  }, 4000)
+  //const devProcess = await wc.spawn('npm', ['run', 'dev'])
+  //devProcessRef.current = devProcess
+  //devProcess.output.pipeTo(
+    //new WritableStream({
+      //write(data) {
+        //addLog(stripAnsi(data))
+        //if (data.includes('ready in') && iframeRef.current && url) {
+          //iframeRef.current.src = url
+        //}
+      //}
+    //})
+  //
+  //setTimeout(() => {
+    //if (iframeRef.current && url) iframeRef.current.src = url
+  //}, 4000)
 }
 
 
